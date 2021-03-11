c9b485dbea03db10163093ad3a68bf7dd7e61198
Cristian Furtado <cristiansafurtado@gmail.com>
2021-03-11 09:48:29 -0300

    Add Breadcrumb in all pages but the Home page and add Breadcrumb.css for styling
    Signed-off-by: Cristian Furtado <cristiansafurtado@gmail.com>

--- src/screens/DeputyProfileScreen.js
+++ src/screens/DeputyProfileScreen.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import { Breadcrumb, BreadcrumbItem } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '../components/Breadcrumb/breadcrumb.css';
 
 function DeputyProfileScreen(props) {
   const { match } = props;
@@ -6,6 +9,11 @@ function DeputyProfileScreen(props) {
 
   return (
     <main>
+      <Breadcrumb>
+        <BreadcrumbItem href="/">Página Inicial</BreadcrumbItem>
+        <BreadcrumbItem href="/deputados">Lista de Deputados</BreadcrumbItem>
+        <BreadcrumbItem active>{id}</BreadcrumbItem>
+      </Breadcrumb>
       <h1>
         Deputado
         {id}
