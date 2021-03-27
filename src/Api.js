06757cf033b7c2f7247b5cf9e90c13ce51172790
Gabriel Albino <enggabrielalbino@gmail.com>
2021-03-27 14:40:43 -0300

    Fixing import

--- src/Api.js
+++ src/Api.js
@@ -3,5 +3,6 @@ export const basePort = '8001';
 export const apiURL = `${baseURL}:${basePort}`;
 export const camaraURL = `${apiURL}/camara`;
 export const camaraSearchRoute = `${camaraURL}/resultado`;
+export const deputadosHomeRoute = `${camaraURL}/deputies-home`;
 export const partiesSearchRoute = `${camaraURL}/parties`;
 export const ufSearchRoute = `${camaraURL}/federative_unities`;
