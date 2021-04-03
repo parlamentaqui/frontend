f152aae1242f6fd6c958a18c56a09030527f1d09
erteduarda <erteduarda@gmail.com>
2021-04-03 14:19:52 -0300

    #3 Implementing new deputy profile information page

--- src/screens/DeputyProfileScreen.js
+++ src/screens/DeputyProfileScreen.js
@@ -1,100 +1,52 @@
 import React from 'react';
-import { Row, Col, Container } from 'react-bootstrap';
+import { Container, Row, Col } from 'react-bootstrap';
+import Profile from '../images/perfil.png';
+import IconInsta from '../images/insta.png';
+import IconFace from '../images/face.png';
+import IconEmail from '../images/email.png';
+import IconTwitter from '../images/twitter.png';
+import IconShare from '../images/share.png';
 import '../css/DeputyProfileScreen.css';
-import Image from 'react-bootstrap/Image';
-import ProfileImage from '../img/perfil.png';
-import IconFace from '../img/icon-face.png';
-import IconInsta from '../img/icon-insta.png';
-import IconTT from '../img/icon-tt.png';
 
 function DeputyProfileScreen() {
   return (
     <main>
       <Container>
-        <Row>
-          {/* Coluna da esquerda / demais informações */}
-          <Col>
-            <Row>
-              <Col>
-                {/* Linha com nome e exercicio */}
-                <Row>
-                  <div>
-                    <h4 className="nameDeputy">ERIKA KOKAY</h4>
-                    <p>TITULAR EM EXERCÍCIO 2019 - 2023</p>
-                  </div>
-                </Row>
-                {/* Linha para demais informações */}
-                <Row>
-                  <div className="divInfos">
-                    <p className="othersInfo" />
-                    DEMAIS INFORMAÇÕES
-                  </div>
-                </Row>
-              </Col>
-            </Row>
+        <Row className="background-div">
+          <Col md="4" className="d-flex justify-content-center align-items-center">
+            <img src={Profile} alt="Profile" className="img-arredondada" />
           </Col>
-          {/* Coluna da direita / informações pontuais */}
-          <Col lg="4" className="divInfoComplete">
-            <Row>
-              <Col className="colImage"><Image src={ProfileImage} fluid /></Col>
-            </Row>
-            <Row>
-              <Col className="rowTitle d-flex justify-content-center">Informações pessoais</Col>
-            </Row>
-            <Row>
-              <Col lg="6">
-                <div className="colInfo">
-                  <p>Nome</p>
-                  <p>Partido</p>
-                  <p>Estado</p>
-                  <p>Idade</p>
-                </div>
+          <Col md="8">
+            <Row className="tam-row-name">
+              <Col md="10">
+                <h4><b>ERIKA KOKAY</b></h4>
+                <h4>TITULAR EM EXERCÍCIO 2019 - 2023</h4>
               </Col>
-              <Col lg="6">
-                <div className="colInfo1">
-                  <p>Érika Jucá Kokay</p>
-                  <p>PT</p>
-                  <p>Distrito Federal</p>
-                  <p>56 (15/08/1957)</p>
-                </div>
+              <Col md="2" className="d-flex justify-content-end align-items-start">
+                <img src={IconShare} alt="Share" className="icon-share" />
               </Col>
             </Row>
-            <Row>
-              <Col className="rowTitle d-flex justify-content-center">Informações do Gabinete</Col>
-            </Row>
-            <Row>
-              <Col lg="6">
-                <div className="colInfo">
-                  <p>Número da sala</p>
-                  <p>Andar</p>
-                  <p>Prédio</p>
-                  <p>Telefone</p>
-                  <p>Email</p>
-                </div>
+            <Row className="tam-row-info">
+              <Col md="6">
+                <h5>Informações pessoais</h5>
+                <h6><b>Nome:</b></h6>
+                <h6><b>Partido:</b></h6>
+                <h6><b>Estado:</b></h6>
+                <h6><b>Idade:</b></h6>
               </Col>
-              <Col lg="6">
-                <div className="colInfo1">
-                  <p>234</p>
-                  <p>2</p>
-                  <p>4</p>
-                  <p>3215-5203</p>
-                  <p>dep.erikakokay@camara.leg.br</p>
-                </div>
+              <Col md="6" className="col-info">
+                <h5>Informações do gabinete</h5>
+                <h6><b>Número da sala:</b></h6>
+                <h6><b>Andar:</b></h6>
+                <h6><b>Prédio:</b></h6>
+                <h6><b>Telefone:</b></h6>
               </Col>
             </Row>
-            <Row>
-              <Col className="rowTitle d-flex justify-content-center">Informações do Gabinete</Col>
-            </Row>
-            <Row className="rowSocial">
-              <Col className="d-flex justify-content-center align-items-center">
-                <Image className="icon" src={IconFace} />
-              </Col>
-              <Col className="d-flex justify-content-center align-items-center">
-                <Image className="icon" src={IconInsta} />
-              </Col>
-              <Col className="d-flex justify-content-center align-items-center">
-                <Image className="icon" src={IconTT} />
-              </Col>
+            <Row className="tam-row-social d-flex justify-content-center align-items-center">
+              <img src={IconEmail} alt="Email" className="icon-email" />
+              <img src={IconInsta} alt="Insta" className="icon-insta" />
+              <img src={IconFace} alt="Face" className="icon-face" />
+              <img src={IconTwitter} alt="Twitter" className="icon-tt" />
             </Row>
           </Col>
         </Row>
