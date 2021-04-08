7a4c77fdb4f174148220f75c1afb7acb4605c476
CibeleG <cibelegoudinho13@gmail.com>
2021-04-08 18:53:24 -0300

    #31 Fixing conflict
    Signed-off-by: CibeleG <cibelegoudinho13@gmail.com>

--- src/components/ShareButton/ShareButton.js
+++ src/components/ShareButton/ShareButton.js
@@ -1,20 +1,16 @@
 import React from 'react';
 import { OverlayTrigger, Button, Popover } from 'react-bootstrap';
 import './ShareButton.css';
-// import { useHistory } from 'react-router-dom';
 
 function ShareButton(props) {
-  const { deputyName, newsTitle, link } = props;
-  // const history = useHistory();
+  const { message, link } = props;
   const handleTweetShare = () => {
-    const shareMessage = `Confira a notícia sobre o deputado ${deputyName}: ${newsTitle} Via parlamentaqui.com/#news`;
-    const twitterString = `https://twitter.com/share?url=${link}&text=${shareMessage}`;
+    const twitterString = `https://twitter.com/share?url=${link}&text=${message}`;
     const win = window.open(twitterString, '_blank');
     win.focus();
   };
   const handleWppShare = () => {
-    const shareMessage = `Confira a notícia sobre o deputado ${deputyName}: ${newsTitle} Via parlamentaqui.com/#news`;
-    const wppString = `https://api.whatsapp.com/send?text=${shareMessage}`;
+    const wppString = `https://api.whatsapp.com/send?text=${message}`;
     const win = window.open(wppString, '_blank');
     win.focus();
   };
@@ -35,11 +31,9 @@ function ShareButton(props) {
             <Popover id={`popover-positioned-${placement}`}>
               <Popover.Content className="cor">
                 <Button onClick={handleWppShare}>
-                  {/* <a href={`whatsapp://send?text=${urlAtual}`} data-action="share/whatsapp/share" onClick="javascript:window.open(this.href, '', 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=300,width=600');return false;" target="_blank" title="Share on whatsapp" /> */}
                   <img src="/images/WhatsappIcon.svg" className="d-inline-block imagem" alt="WhatsappButton" />
                 </Button>
                 <Button onClick={handleFaceShare}>
-                  {/* <a href={`https://www.facebook.com/sharer/sharer.php?u=${urlAtual}&t=<Confira esta notícia sobre {sexo ? 'o' : 'a'} deputadx nome>`} onClick="javascript:window.open(this.href, '', 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=300,width=600');return false;" target="_blank" title="Share on Facebook" /> */}
                   <img src="/images/FacebookIcon.svg" className="d-inline-block imagem" alt="FacebookButton" />
                 </Button>
                 <Button onClick={handleTweetShare}>
