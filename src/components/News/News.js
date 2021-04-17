cd969189e4b182aa6efb1641bcd9595948647116
erteduarda <erteduarda@gmail.com>
2021-04-17 01:14:11 -0300

    Inserindo botão de compartilhar notícias no perfil do deputado. Co-authored-by CibeleG <cibelegoudinho13@gmail.com>
    Signed-off-by: erteduarda <erteduarda@gmail.com>

--- src/components/News/News.js
+++ src/components/News/News.js
@@ -3,13 +3,15 @@ import axios from 'axios';
 import { useHistory } from 'react-router-dom';
 import { CardGroup, Col, Row } from 'react-bootstrap';
 import './News.css';
+import ShareButton from '../ShareButton/index';
 import { deputyNewsRoute } from '../../Api';
 
 function News() {
   const history = useHistory();
   const id = history.location.pathname.split('/')[2];
   const [news, setNews] = useState([]);
-
+  const shareLink = (element) => element.link;
+  const shareMessage = (element) => `Confira a notícia sobre o deputado ${element.deputy_name}: ${element.title}%0aVia parlamentaqui.com`;
   useEffect(async () => {
     axios.get(deputyNewsRoute(id)).then((response) => {
       setNews(response.data);
@@ -33,6 +35,12 @@ function News() {
                 <h12 className="text-news-font">{element.source}</h12>
                 <h5 className="text-news-title">{element.title}</h5>
                 <p className="text-news-body">{element.abstract}</p>
+              </Col>
+              <Col md="2" className="d-flex align-items-start">
+                <ShareButton
+                  message={shareMessage(element)}
+                  link={shareLink(element)}
+                />
               </Col>
             </Row>
           ))}
