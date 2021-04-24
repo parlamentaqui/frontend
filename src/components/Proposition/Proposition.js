0b53697b29645cf05ba5640974bbe1731483efda
Rafael-gc <rafaelfernandeszk@gmail.com>
2021-04-23 23:47:23 -0300

    #59 Criado caixa do autor da proposição, alterações no .css Co-authored-by: Cristian Furtado <cristiansafurtado@gmail.com>
    Signed-off-by: Rafael-gc <rafaelfernandeszk@gmail.com>

--- src/components/Proposition/Proposition.js
+++ src/components/Proposition/Proposition.js
@@ -10,20 +10,32 @@ function Proposition(props) {
   const { propo } = props;
 
   return (
-    <div>
+    <Container>
       <Row>
-        <h1>Projeto de Lei 14/1999</h1>
-        <Container classname="propThemeBox">
-          <p>Trabalho e Emprego</p>
-        </Container>
-        <Container className="propAuthorBox">
-          <img src={AuthorPhoto} alt="FotoAutorProp" className="icon-author" />
-          <p className="authorNameInfo">Autoria: Erica Kokay</p>
-          <p className="authorPartyAndRegionInfo">PT - DF</p>
-        </Container>
+        <Col md="12" lg="6">
+          <h1>Projeto de Lei 14/1999</h1>
+        </Col>
+        <Col className="propAuthorBox" md="12" lg="6">
+          <Row>
+            <img src={AuthorPhoto} alt="FotoAutorProp" className="icon-author" />
+            <div>
+              <p className="authorNameInfo">Autoria: Erica Kokay</p>
+              <p className="authorPartyAndRegionInfo">PT - Distrito Federal</p>
+            </div>
+          </Row>
+        </Col>
       </Row>
-      {propo.ementa}
-    </div>
+
+      {/* <Container classname="propThemeBox">
+        <p>{propo.tema_proposicao}</p>
+      </Container> */}
+      {/* <Container className="propAuthorBox">
+        <img src={AuthorPhoto} alt="FotoAutorProp" className="icon-author" />
+        <p className="authorNameInfo">Autoria: Erica Kokay</p>
+        <p className="authorPartyAndRegionInfo">PT - DF</p>
+      </Container> */}
+      {/* {propo.ementa} */}
+    </Container>
   );
 }
 
