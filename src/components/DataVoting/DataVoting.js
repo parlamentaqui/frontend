755b45ae33f0c3e9b9cc6363104d8eb056378ed4
lmmLucasMachado <39622092+lmmLucasMachado@users.noreply.github.com>
2021-05-01 19:18:33 -0300

    Revert "Devel"

--- src/components/DataVoting/DataVoting.js
+++ src/components/DataVoting/DataVoting.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 import { useHistory } from 'react-router-dom';
 import './DataVoting.css';
 import { Row, Col } from 'react-bootstrap';
+import IconShareBlack from '../../images/share-black.png';
 import IconVoto from '../../images/votacao.png';
 import IconConfirma from '../../images/icon-confirma.png';
 import IconConfirmaBlack from '../../images/icon-confirma-black.png';
@@ -26,7 +27,7 @@ export function defineDate(date) {
 export function defineVote(vote) {
   const str = String(vote);
   const shareMessage = `Confira esse voto sobre ${vote.deputy_name} Via parlamentaqui.com`;
-  if (!vote.localeCompare('Sim')) {
+  if (vote.localeCompare('Sim')) {
     return (
       <div>
         <img
@@ -60,6 +61,7 @@ function DataVoting() {
   useEffect(async () => {
     const result = await axios(voteRoute(id));
     setVotes(result.data);
+
     // useEffect(() => {
     //   axios.get(voteRoute(id)).then((response) => {
     //     setVotes(response.data);
