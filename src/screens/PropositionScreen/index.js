2395d4d987f5aa0ef5317c6f19c8798d1a028d74
jplyra <jplyra97@gmail.com>
2021-05-07 18:31:35 -0300

    #70 componente final de tweets da página de proposições.
    Signed-off-by: jplyra <jplyra97@gmail.com>

--- src/screens/PropositionScreen/index.js
+++ src/screens/PropositionScreen/index.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { Row, Col, Container, Image } from 'react-bootstrap';
 import { useHistory, useParams } from 'react-router-dom';
-import { propositionRoute } from '../../Api';
+import { propositionRoute, tweetsPropositionRoute } from '../../Api';
 import Proposition from '../../components/Proposition/Proposition';
 import PropMobile from '../../components/Proposition/PropositionMobile';
 import './index.css';
@@ -12,16 +12,15 @@ function PropositionScreen() {
   const history = useHistory();
   const id = history.location.pathname.split('/')[2];
   const [proposition, setProposition] = useState([]);
-  // ToDo: const dos tweets
-  const [tweet, setTweet] = useState([]);
+  const [tweets, setTweets] = useState([]);
   useEffect(() => {
     axios.get(propositionRoute(id)).then((response) => {
       setProposition(response.data);
     });
-    // ToDo: axios p tweets
-    // axios.get(rotaDosTweets(const dos tweets)).then((response) => {
-    //   setDeputy(response.data);
-    // });
+    axios.get(tweetsPropositionRoute(id)).then((response) => {
+      setTweets(response.data);
+      console.log(response.data);
+    });
   }, []);
 
   return (
@@ -40,16 +39,12 @@ function PropositionScreen() {
         <Row>
           <Col md={{ span: 6, offset: 6 }}>
             <h2 className="mb-3">Tweets</h2>
-            {/* ToDo: */}
-            {/* props p passar dentro do componente tweet: tweets={tweets}  */}
-            {/* ToDo: logica p ver se tem tweets. Verifica se o array está vazio */}
-            {/* caso esteja retorna div de vazio, caso n retorna tweets */}
-            {tweet.length === 0 ? (
+            {tweets.length === 0 ? (
               <div className="no-tweets pl-2">
                 <p>Não existem tweets.</p>
               </div>
             ) : (
-              <Tweet />
+              <Tweet tweets={tweets} />
             )}
           </Col>
         </Row>
