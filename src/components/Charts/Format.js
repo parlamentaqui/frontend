c708c52d8e87531470b6ca36c78c42566040164b
CibeleG <cibelegoudinho13@gmail.com>
2021-05-08 21:39:48 -0300

    #72 igualando grafico ao prototipo do figma
    Signed-off-by: CibeleG <cibelegoudinho13@gmail.com>

--- src/components/Charts/Format.js
+++ src/components/Charts/Format.js
@@ -7,13 +7,13 @@ function dataF(dados) {
       <Col xs={4} className="col-esp mr-3" title={dados.label}>
         {dados.label}
       </Col>
-      <Col xs={2} className="col-esp ml-3" title={dados.value} color="#000000">
+      <Col xs={2} className="col-esp ml-3" title={dados.value}>
         R$
         {' '}
         {dados.value}
       </Col>
       <Col md="2" className="circle-align">
-        <div className="circle ml-3" />
+        <div className="circle ml-3" style={{ backgroundColor: dados.color }} />
       </Col>
       <Col xs={4} className="col-esp2" title={dados.percentage}>
         {dados.percentage.toFixed(1)}
@@ -27,17 +27,15 @@ function Format(props) {
   const { dados } = props;
   const { name } = props;
   const { total } = props;
-  const slices = {};
-  dados.forEach((element, index) => {
-    slices[index] = { color: element.color };
-  });
   return (
     <>
       <div>
         <Row className="col-data2">
           <h5>{name.name}</h5>
         </Row>
-        <Row>{dados.map((element) => dataF(element))}</Row>
+        <Row>
+          {dados.map((element) => dataF(element))}
+        </Row>
         <Row className="col-data3">
           <Col xs={4} className="col-esp mr-3" title="Total">
             <b>TOTAL</b>
