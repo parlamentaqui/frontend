48550ab37bcc45b06ca2ef41eef8c9593a383d60
Cristian Furtado <cristiansafurtado@gmail.com>
2021-05-11 09:26:57 -0300

    #86 Importa componente DeputiesList
    Co-authored-by: Cristian Furtado <cristiansafurtado@gmail.com>
    Co-authored-by: Eduarda Tavares <erteduarda@gmail.com>
    Signed-off-by: Cristian Furtado <cristiansafurtado@gmail.com>

--- src/screens/DeputiesScreen.js
+++ src/screens/DeputiesScreen.js
@@ -1,10 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Container } from 'react-bootstrap';
+import DeputiesList from '../components/DeputiesList';
+import { deputadosHomeRoute } from '../Api';
 
 function DeputiesScreen() {
+  const [deputies, setDeputies] = useState([]);
+
+  useEffect(() => {
+    axios.get(deputadosHomeRoute).then((response) => {
+      setDeputies(response.data);
+    });
+  }, []);
+
   return (
     <main>
-      <h1>Lista de deputados</h1>
+      <Container>
+        <h1>Lista de deputados</h1>
+        <DeputiesList deputados={deputies} theme="dark" />
+      </Container>
     </main>
   );
 }
